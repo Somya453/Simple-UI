f090bc275a9846f5300358db4b1dc1d8f1694cf6
somyaa <somyasharma0488@gmail.com>
2025-11-19 19:42:31 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import Button from './components/Button.jsx'
+import Header from './components/Header.jsx'
+import Hero from './components/Hero.jsx'
+import Footer from './components/Footer.jsx'
+
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
